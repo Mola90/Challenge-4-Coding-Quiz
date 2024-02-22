16addff7d42be57aa3920762918d8906bf732efd
Mola Dafa <mola_d@hotmail.com>
2024-02-23 09:54:04 +1100

    timer set up

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
- 
+ var timer = document.querySelector("#timer");
+ var timerCount = 10;
+ var startButton = document.querySelector(".button-start");
+
+ // count down timer function
+ console.log(timerCount);
+
+ function startGame(){
+
+   setInterval(function(){
+      timerCount--;
+      timer.textContent = timerCount;
+      console.log(timerCount);
+  
+  
+   },1000);
+
+ }
+
+
+
+ startButton.addEventListener("click", function(){
+   startGame();
+
+ });
